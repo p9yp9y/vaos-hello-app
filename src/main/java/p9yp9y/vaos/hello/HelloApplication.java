f62f60b16dffe0f8ee5258137ac089d40058e042
Andris <dobrosi@gmail.com>
2017-11-17 18:09:40 +0000

    ok

--- src/main/java/p9yp9y/vaos/hello/HelloApplication.java
+++ src/main/java/p9yp9y/vaos/hello/HelloApplication.java
@@ -3,13 +3,15 @@ package p9yp9y.vaos.hello;
 import de.steinwedel.messagebox.MessageBox;
 import p9yp9y.vaos.VaosApplication;
 
-public class HelloApplication extends VaosApplication {
-
-	@Override
+public class HelloApplication implements VaosApplication {
+    @Override
 	public void main(String[] args) {
 		System.out.println("Hello!");
-
 		MessageBox.createInfo().withCaption("Info").withMessage("Hello!").withOkButton().open();
 	}
-
+	
+    @Override	
+	public String getName() {
+	    return "Hello!";
+	}
 }
